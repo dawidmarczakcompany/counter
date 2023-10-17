60f22127cc1fee30aa0f29bbf50271bcdc8f8ad6
dawidmarczakcompany <145110151+dawidmarczakcompany@users.noreply.github.com>
2023-10-17 16:56:04 +0200

    feature: add minus button with decrement feature (#6)
    Co-authored-by: Dawid Marczak <dawidmarczak@Dawids-MBP.lan>

--- pages/CounterPage/index.tsx
+++ pages/CounterPage/index.tsx
@@ -3,14 +3,16 @@ import styles from "./styles";
 import { useDispatch, useSelector } from "react-redux";
 import { selectCounterValue } from "../../store/counter/selectors";
 import Button from "../../components/Button";
-import { increaseCounter } from "../../store/counter/actionts";
-import { MAX_COUNTER_VALUE } from "../../contants";
+import { decreaseCounter, increaseCounter } from "../../store/counter/actions";
+import { MAX_COUNTER_VALUE, MIN_COUNTER_VALUE } from "../../constants";
 
 const CounterPage = () => {
   const counterValue = useSelector(selectCounterValue);
   const dispatch = useDispatch();
 
   const onCounterIncrease = () => dispatch(increaseCounter());
+
+  const onCounterDecrease = () => dispatch(decreaseCounter());
 
   return (
     <View style={styles.pageWrapper}>
@@ -19,6 +21,12 @@ const CounterPage = () => {
       <Text>{counterValue}</Text>
 
       <View style={styles.buttonsWrapper}>
+        <Button
+          onPress={onCounterDecrease}
+          label="Minus"
+          disabled={counterValue === MIN_COUNTER_VALUE}
+        />
+
         <Button
           onPress={onCounterIncrease}
           label="Plus"
--- store/counter/actions.ts
+++ store/counter/actions.ts
@@ -3,3 +3,7 @@ import { CounterActionType } from "./types";
 export const increaseCounter = () => ({
   type: CounterActionType.INCREASE_COUNTER,
 });
+
+export const decreaseCounter = () => ({
+  type: CounterActionType.DECREASE_COUNTER,
+});
